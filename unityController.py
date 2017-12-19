7bfa5686dae4658af3685850c3d2cbb3cff8a517
Trent <trent@torkar.me>
2017-12-19 15:46:43 +1030

    Modified controller

--- unityController.py
+++ unityController.py
@@ -3,7 +3,8 @@
 
 import socket
 import json
-import io
+import threading
+import struct
 
 host = 'localhost' 
 port = 50000
@@ -13,18 +14,33 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port)) 
 s.listen(backlog) 
 
+def function(client, address):
+    while True:
+        length = client.recv(1024).decode()
+        data = b''
+        while len(data) < length:
+            # doing it in batches is generally better than trying
+            # to do it all in one go, so I believe.
+            to_read = length - len(data)
+            data += client.recv(4096 if to_read > 4096 else to_read)
+        print(data)
+        
+#    data = client.recv(size)
+#    if data:
+##            print(data.decode())
+#        try:
+#            json_data = json.loads(data.decode())
+#            print(json_data)
+#        except:
+#            print('\n\nUnable to read: \n\n', data)
+
+    
+
 while True:
     print('Waiting for Connection')
     client, address = s.accept() 
-    print ("Client connected.")
-    while True:
-        data = client.recv(size)
-        if data:
-#            print(data.decode())
-            try:
-                json_data = json.loads(data.decode())
-                print(json_data)
-            except:
-                print('\n\nUnable to read: \n\n', data)
-    
+    print ("Client {} connected on {}.".format(client, address))
+    tmpThread = threading.Thread(target=function, args=(client, address)) #  If the file is received ， Create thread 
+    tmpThread.start() #  Execution thread 
+
                
